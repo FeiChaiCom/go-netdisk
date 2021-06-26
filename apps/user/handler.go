0f2f34caeaf60ed92d7e6c61168c217a89aa4a1f
gaomugong <84143485+gaomugong@users.noreply.github.com>
2021-06-26 23:41:16 +0800

    feature: add login req middleware

--- apps/user/handler.go
+++ apps/user/handler.go
@@ -3,7 +3,7 @@ package user
 import (
 	"encoding/json"
 	"github.com/gin-gonic/gin"
-	"github.com/go-playground/validator/v10"
+	"github.com/go-playground/validator"
 	"log"
 	"net/http"
 	"strings"
@@ -11,9 +11,10 @@ import (
 )
 
 type User struct {
-	Name string `form:"name" json:"name"`
-	Age  int    `form:"age" json:"age" binding:"required"`
-	//BirthDay time.Time `json:"birthday" binding:"required"`
+	Name string `form:"name" json:"name" binding:"required"`
+	Age  int    `form:"age" json:"age" binding:"required,gt=10"`
+	// time_format has issue when set content-type to application/json, but work with form style
+	BirthDay time.Time `form:"birthday" json:"birthday" time_format:"2006-01-02 15:04:05"`
 }
 
 // TODO: parsing time "2021-05-27 15:04:05" as "2006-01-02T15:04:05Z07:00": cannot parse " 15:04:05" as "T"
@@ -22,19 +23,20 @@ var birthDayValidator validator.Func = func(fl validator.FieldLevel) bool {
 	log.Println(birthDay, ok)
 	if ok {
 		today := time.Now()
-		if today.Before(birthDay) {
-			return false
-		}
-		return true
+		return !today.Before(birthDay)
 	}
 	return false
 }
 
-//curl  curl http://localhost:5000/api/users/test_post/ -X POST -d '{"name": "miya", "age": 12}'
+// curl http://localhost:5000/api/users/test_post/ -X POST -d '{"name": "miya", "age": 11}' -H 'Content-Type: application/json' -vvv
+// curl http://localhost:5000/api/users/test_post/ -X POST -F name=miya -F age=20 -F birthday="2020-11-12 11:11:11"
+// curl http://localhost:5000/api/users/test_post/ -X POST -d '{"name": "miya", "age": 1, "birthday": "2020-01-02T11:12:13.123433"}' -H 'Content-Type: application/json' -vvv
 func testPostUserData(c *gin.Context) {
 	var user User
 
-	if err := c.ShouldBindJSON(&user); err != nil {
+	// if err := c.ShouldBindJSON(&user); err != nil {
+	// ShouldBind will guess which content-type is the request and call the specific bind type, eg. bindJSON
+	if err := c.ShouldBind(&user); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"result":  false,
 			"message": err.Error(),
@@ -42,15 +44,15 @@ func testPostUserData(c *gin.Context) {
 		return
 	}
 
-	userJson, _ := json.Marshal(user)
-	log.Println(string(userJson))
+	userJSON, _ := json.Marshal(user)
+	log.Println(string(userJSON))
 	c.JSON(http.StatusOK, gin.H{
 		"result": true,
 		"data":   user,
 	})
 }
 
-//curl http://localhost:5000/api/users/test_post_form/ -X POST -F name=miya -F age=12
+// curl http://localhost:5000/api/users/test_post_form/ -X POST -F name=miya -F age=12
 func testPostUser(c *gin.Context) {
 	name := c.DefaultPostForm("name", "pitou")
 	age := c.PostForm("age")
@@ -65,7 +67,6 @@ func testPostUser(c *gin.Context) {
 
 // curl http://localhost:5000/api/users/test_get/1/eat/?age=12
 func testGetUser(c *gin.Context) {
-
 	name := c.DefaultQuery("name", "pitou")
 	age := c.Query("age")
 	uid := c.Param("uid")
@@ -81,11 +82,12 @@ func testGetUser(c *gin.Context) {
 
 	c.JSON(http.StatusOK, gin.H{
 		"result": true,
-		"data": map[string]interface{}{
-			"name": name,
-			"age":  age,
-			"uid":  uid,
-			"list": []string{"zhangsan", "lisi"},
+		"data": gin.H{
+			"name":   name,
+			"age":    age,
+			"uid":    uid,
+			"action": action,
+			"list":   []string{"zhangsan", "lisi"},
 		},
 	})
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/gin-gonic/gin v1.7.2
+	github.com/go-playground/validator v9.31.0+incompatible // indirect
 	github.com/go-playground/validator/v10 v10.4.1
 	github.com/mattn/go-isatty v0.0.13 // indirect
 	golang.org/x/sys v0.0.0-20210616094352-59db8d763f22 // indirect
