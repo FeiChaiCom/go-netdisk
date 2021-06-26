03685964b247410ce374f3efe7d757d611b02de6
gaomugong <84143485+gaomugong@users.noreply.github.com>
2021-06-26 09:32:39 +0800

    feature: test get/post api

--- routers/users.go
+++ routers/users.go
@@ -3,17 +3,46 @@ package routers
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strings"
 )
 
 // Add user apis to api group
 func registerUserGroup(rg *gin.RouterGroup) {
 	monitors := rg.Group("/users/")
-	monitors.GET("", userList)
+	monitors.GET("test_get/:uid/*action", testGetUser)
+	monitors.POST("test_post/", testPostUser)
 }
 
-func userList(c *gin.Context) {
+//curl http://localhost:5000/api/users/test_post/ -X POST -F name=miya -F age=12
+func testPostUser(c *gin.Context) {
+	name := c.DefaultPostForm("name", "pitou")
+	age := c.PostForm("age")
+
 	c.JSON(http.StatusOK, gin.H{
 		"result": true,
-		"data":   []string{"zhangsan", "lisi"},
+		"data": map[string]interface{}{
+			"name": name,
+			"age":  age,
+		},
+	})
+}
+
+// http://localhost:5000/api/users/test_get/1/eat/?age=12
+func testGetUser(c *gin.Context) {
+
+	name := c.DefaultQuery("name", "pitou")
+	age := c.Query("age")
+	uid := c.Param("uid")
+	action := c.Param("action")
+	action = strings.Trim(action, "/")
+
+	c.JSON(http.StatusOK, gin.H{
+		"result": true,
+		"data": map[string]interface{}{
+			"name": name,
+			"age":  age,
+			"uid":  uid,
+			"list": []string{"zhangsan", "lisi"},
+		},
 	})
 }
