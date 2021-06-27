ecc85a3e5247b609189c24047cd6039f6765b0c8
gaomugong <84143485+gaomugong@users.noreply.github.com>
2021-06-27 13:11:36 +0800

    feature: test gracefully shutdown

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/gaomugong/go-netdisk/apps"
 	cfg "github.com/gaomugong/go-netdisk/config"
@@ -9,9 +10,16 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
+	// Create context that listens for the interrupt signal from the OS.
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
 	// Init url router for apis
 	router := apps.InitAPIRouter()
 
@@ -30,7 +38,37 @@ func main() {
 	// Serve media files
 	router.StaticFS(cfg.MediaURL, http.Dir(cfg.MediaDir))
 
-	_ = router.Run(fmt.Sprintf(":%d", cfg.Port))
+	// _ = router.Run(fmt.Sprintf(":%d", cfg.Port))
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%d", cfg.Port),
+		Handler: router,
+	}
+
+	// Initializing the server in a goroutine so that
+	// it won't block the graceful shutdown handling below
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	// Listen for the interrupt signal.
+	<-ctx.Done()
+
+	// Restore default behavior on the interrupt signal and notify user of shutdown.
+	stop()
+	log.Println("shutting down gracefully, press Ctrl+C again to force")
+
+	// The context is used to inform the server it has 5 seconds to finish
+	// the request it is currently handling
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown: ", err)
+	}
+
+	log.Println("Server exiting")
 }
 
 // Init gin log to file and stdout
