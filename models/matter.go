c05d15b6b148fef2646a043837b48b8ee3eb449c
gaomugong <84143485+gaomugong@users.noreply.github.com>
2021-06-30 02:09:29 +0800

    feature: matter接口编写

--- models/matter.go
+++ models/matter.go
@@ -1,26 +1,27 @@
 package models
 
 import (
-	"database/sql"
+	"fmt"
 	cfg "github.com/gaomugong/go-netdisk/config"
+	"math"
 	"time"
 )
 
 type Matter struct { //nolint:maligned
-	UUID       string         `gorm:"column:uuid;primaryKey;type:varchar(36)"`
-	PUUID      string         `gorm:"column:puuid;type:varchar(36) not null"`
-	Name       string         `gorm:"column:name;type:varchar(255) not null"`
-	UserName   string         `gorm:"column:username;type:varchar(45) not null"`
-	UserUUID   string         `gorm:"column:user_uuid;type:varchar(36) not null"`
-	Md5        sql.NullString `gorm:"column:md5;type:varchar(45)"`
-	Size       int            `gorm:"column:size;not null"`
-	Dir        bool           `gorm:"column:dir;not null;default:false"`
-	Privacy    bool           `gorm:"column:privacy;not null;default:true"`
-	Path       string         `gorm:"column:path;type:varchar(1024)"`
-	UpdateTime time.Time      `gorm:"column:update_time;not null"`
-	CreateTime time.Time      `gorm:"column:create_time;not null"`
-	Times      uint32         `gorm:"column:times;not null"`
-	File       string         `gorm:"column:file;type:varchar(100) not null"`
+	UUID       string    `gorm:"column:uuid;primaryKey;type:varchar(36)" json:"uuid"`
+	PUUID      string    `gorm:"column:puuid;type:varchar(36) not null" json:"puuid"`
+	Name       string    `gorm:"column:name;type:varchar(255) not null" json:"name"`
+	UserName   string    `gorm:"column:username;type:varchar(45) not null" json:"username"`
+	UserUUID   string    `gorm:"column:user_uuid;type:varchar(36) not null" json:"userUuid"`
+	Md5        string    `gorm:"column:md5;type:varchar(45)" json:"md5"`
+	Size       int       `gorm:"column:size;not null" json:"size"`
+	Dir        bool      `gorm:"column:dir;not null;default:false" json:"dir"`
+	Privacy    bool      `gorm:"column:privacy;not null;default:true" json:"privacy"`
+	Path       string    `gorm:"column:path;type:varchar(1024)" json:"path"`
+	UpdateTime time.Time `gorm:"column:update_time;not null" json:"updateTime"`
+	CreateTime time.Time `gorm:"column:create_time;not null" json:"createTime"`
+	Times      uint32    `gorm:"column:times;not null" json:"times"`
+	File       string    `gorm:"column:file;type:varchar(100) not null" json:"file"`
 }
 
 func (Matter) TableName() string {
@@ -28,5 +29,43 @@ func (Matter) TableName() string {
 }
 
 func init() {
-	cfg.DB.AutoMigrate(&Matter{})
+	// cfg.DB.AutoMigrate(&Matter{})
+}
+
+func GetAllMatters(puuid string, name string, page int, pageSize int, order string) (matters []*Matter, totalItems int64, totalPage int) {
+	// cfg.DB.Model(&Matter{}).Where(Matter{PUUID: puuid}).Count(&totalItems)
+	tx := cfg.DB.Model(&Matter{}).Where("puuid = ?", puuid)
+
+	if name != "" {
+		tx = tx.Where("name LIKE ?", fmt.Sprintf("%s%s%s", "%", name, "%"))
+	}
+
+	tx.Count(&totalItems)
+
+	switch {
+	case pageSize > 100:
+		pageSize = 100
+	case pageSize <= 0:
+		pageSize = 10
+	}
+
+	totalPage = int(math.Round(float64((totalItems + int64(pageSize) - 1) / int64(pageSize))))
+	if page > totalPage {
+		page = 1
+	}
+
+	if page > 0 {
+		if offset := (page - 1) * pageSize; offset > 0 {
+			tx = tx.Offset(offset)
+		}
+	}
+
+	if order != "" {
+		order = fmt.Sprintf("create_time %s", order)
+		tx = tx.Order(order)
+	}
+
+	tx.Limit(pageSize).Find(&matters)
+
+	return
 }
