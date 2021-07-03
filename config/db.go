518d366c610dcebcdca013b34ba790cda55c47b0
gaomugong <84143485+gaomugong@users.noreply.github.com>
2021-07-03 18:06:58 +0800

    feature: add parent to matter detail api

--- config/db.go
+++ config/db.go
@@ -3,6 +3,7 @@ package config
 import (
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 const MYSQL_DSN = "root:root@tcp(localhost:3306)/tank?charset=utf8mb4&parseTime=True&loc=Local"
@@ -11,7 +12,7 @@ var DB *gorm.DB
 
 func InitDB() error {
 	if db, err := gorm.Open(mysql.Open(MYSQL_DSN), &gorm.Config{
-		// Logger: logger.Default.LogMode(logger.Info),
+		Logger: logger.Default.LogMode(logger.Info),
 	}); err != nil {
 		return err
 	} else {
