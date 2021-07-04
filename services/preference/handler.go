7e370b4922293dae3ba23dbe407042193cad6002
gaomugong <84143485+gaomugong@users.noreply.github.com>
2021-07-04 17:42:47 +0800

    feature: add preference

---
+++ services/preference/handler.go
@@ -0,0 +1,17 @@
+package preference
+
+import (
+	cfg "github.com/gaomugong/go-netdisk/config"
+	"github.com/gaomugong/go-netdisk/models/db"
+	R "github.com/gaomugong/go-netdisk/render"
+	"github.com/gin-gonic/gin"
+)
+
+// curl -X POST http://localhost:5000/api/preference/fetch/
+func FetchHandler(c *gin.Context) {
+	var p db.Preference
+	if err := cfg.DB.First(&p).Error; err != nil {
+		R.FailWithError(c, err)
+	}
+	R.Ok(c, p)
+}
