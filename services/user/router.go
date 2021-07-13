f6ed6aaa06fc95368c736e42c3bbc65f3841e08b
gaomugong <84143485+gaomugong@users.noreply.github.com>
2021-07-13 10:09:19 +0800

    debug: add stage1

--- services/user/router.go
+++ services/user/router.go
@@ -7,8 +7,8 @@ import (
 
 // Add user apis to api group
 func RegisterUserGroup(rg *gin.RouterGroup) {
-	// users := rg.Group("/user/").Use(middleware.JWTLoginRequired())
-	users := rg.Group("/user/").Use(middleware.LoginRequired)
+	users := rg.Group("/user/").Use(middleware.JWTLoginRequired())
+	// users := rg.Group("/user/").Use(middleware.LoginRequired)
 	{
 		users.GET("me/", Me)
 		users.GET("page/", PageHandler)
